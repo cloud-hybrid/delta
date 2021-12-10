782653006e08e861d5e4258d825d12fb01f7676d
Jacob B. Sanders <60639231+Segmentational@users.noreply.github.com>
2021-12-10 17:32:35 -0600

    Update generic.js

--- packages/schema-cli/library/generic.js
+++ packages/schema-cli/library/generic.js
@@ -1,4 +1,25 @@
 /***
+ * @name        Generic
+ * @module      @cloud-technology
+ * @summary     .....
+ *
+ * @author      Jacob B. Sanders
+ * @license     BSD 3-Clause License
+ * @copyright   Cloud-Technology LLC. & Affiliates
+ *
+ * @package     {@link https://github.com/cloud-hybrid/delta @cloud-technology}
+ *
+ * @example
+ * >>> import Prompt from "@cloud-technology/cli-prompt";
+ * >>> const Q = await Prompt("Name: ");
+ * >>> console.log(Q);
+ * >>> process.exit(0);
+ *
+ * @returns {Symbol}
+ *
+ */
+
+/***
  *
  * @type {Symbol}
  *
